b138b93c31cc01deddd7658787029073b15bb0f9
Kemaru345 <100782621+Kemaru345@users.noreply.github.com>
2022-03-19 14:46:22 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- Program.cs
+++ Program.cs
@@ -10,6 +10,40 @@ namespace Jurnal_MOD4_1302204079_KPL
     {
         static void Main(string[] args)
         {
+            SimpleDataBase<long> db = new SimpleDataBase<long>();
+            db.AddNewData(13);
+            db.AddNewData(22);
+            db.AddNewData(40);
+            db.PrintAllData();
         }
     }
+
+    class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            this.storedData = new List<T>();
+            this.inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T newData)
+        {
+            this.inputDates.Add(DateTime.Now);
+            this.storedData.Add(newData);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < this.inputDates.Count; i++)
+            {
+                Console.WriteLine("Data " + i + " berisi: " + this.storedData[i] +
+                    ", yang disimpan pada waktu UTC: " + this.inputDates[i]);
+            }
+        }
+    }
+
 }
+
