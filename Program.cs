25564a882108a480a9d1e311b1e20313922727fa
Kemaru345 <100782621+Kemaru345@users.noreply.github.com>
2022-03-19 14:42:21 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal_MOD4_1302204079_KPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
